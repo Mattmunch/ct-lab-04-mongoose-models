029aac69db2db78e3514efdda554477700680a80
Matt Munch <mattmunch94@gmail.com>
2019-12-05 15:33:56 -0800

    car horsepower tested

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -10,5 +10,14 @@ describe('Car Model', () => {
       expect(errors.name.message).toEqual('Path `name` is required.');
     });
   });
+  describe('horsepower', () => {
+    it('requires horsepower', () => {
+      const car = new Car({
+        name:'Nissan GTR'
+      });
+      const { errors } = car.validateSync();
+      expect(errors.horsepower.message).toEqual('Path `horsepower` is required.');
+    });
+  });
 })
 ;

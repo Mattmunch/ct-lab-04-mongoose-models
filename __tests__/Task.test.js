8876599a8083724255459e2a90928e7771db1c6d
Matt Munch <mattmunch94@gmail.com>
2019-12-05 16:18:18 -0800

    forgot to commit

---
+++ __tests__/Task.test.js
@@ -0,0 +1,13 @@
+const Task = require('../lib/models/Task');
+
+describe('Task', () => {
+  describe('title', () => {
+    it('requires a title', () => {
+      const task = new Task({
+        difficulty: 4
+      });
+      const { errors } = task.validateSync();
+      expect(errors.title.message).toEqual('Path `title` is required.');
+    });
+  }); 
+});

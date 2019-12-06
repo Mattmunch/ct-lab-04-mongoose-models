2e328a9d7a07e6d9f5ae8fca0729a6694dbf01b2
Matt Munch <mattmunch94@gmail.com>
2019-12-05 16:32:31 -0800

    complete

--- __tests__/Task.test.js
+++ __tests__/Task.test.js
@@ -18,5 +18,21 @@ describe('Task', () => {
       const { errors } = task.validateSync();
       expect(errors.difficulty.message).toEqual('Path `difficulty` is required.');
     });
+    it('requires difficulty from 1 to 10', () => {
+      const task = new Task({
+        title: 'Take out the trash',
+        difficulty:12
+      });
+      const { errors } = task.validateSync();
+      expect(errors.difficulty.message).toEqual('Path `difficulty` (12) is more than maximum allowed value (10).');
+    });
+    it('requires difficulty from 1 to 10', () => {
+      const task = new Task({
+        title: 'Take out the trash',
+        difficulty:-3
+      });
+      const { errors } = task.validateSync();
+      expect(errors.difficulty.message).toEqual('Path `difficulty` (-3) is less than minimum allowed value (1).');
+    });
   });  
 });
